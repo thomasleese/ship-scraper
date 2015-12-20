6170c10216e8fab00455cb52a62ae6bc9015542d
Thomas Leese <inbox@thomasleese.me>
2015-12-20 16:58:02 +0000

    Add another name replacement

--- scraper.py
+++ scraper.py
@@ -65,7 +65,8 @@ def find_ship_url(mmsi):
             .replace('?', '') \
             .replace('>', '') \
             .replace('<', '') \
-            .replace('$', '')
+            .replace('$', '') \
+            .replace('&', '')
 
         url = 'https://www.vesselfinder.com/vessels/{0}-IMO-{1}-MMSI-{2}' \
             .format(name, data['IMO'], data['MMSI'])
